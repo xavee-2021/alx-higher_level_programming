14e2cbaf18e8e314d4b5fbb82aa5489b8239019d
Ejiro Gabriel <ejirogabriel2021@gmail.com>
2021-12-13 23:41:13 +0000

    code refactor

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let s;
 for (s in strings) {
   console.log(strings[s]);
